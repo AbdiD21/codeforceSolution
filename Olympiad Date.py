0210c2db2cee08086d224d3bee57fe8081b21055
Abdi Dabala <abdid.yadata@gmail.com>
2025-04-05 10:24:11 -0700

    other day

--- Olympiad Date.py
+++ Olympiad Date.py
@@ -1,3 +1,4 @@
+'''
 def can_form_date(drawn_digits):
     target_digits = [0, 1, 0, 3, 2, 0, 2, 5]  # target for "01.03.2025"
     from collections import Counter
@@ -34,4 +35,27 @@ for _ in range(t):
 
 results = olympiad_date(test_cases)
 for res in results:
+    print(res)
+'''
+
+t = int(input())
+required = {0: 3, 1: 1, 3: 1, 2: 2, 5: 1}
+for j in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    counts = {0: 0, 1: 0, 2: 0, 3: 0, 5: 0}
+    res = 0
+    for i in range(n):
+        num = a[i]
+        if num in required:
+            counts[num] += 1
+        # Check if all required counts are met
+        met = True
+        for d in required:
+            if counts[d] < required[d]:
+                met = False
+                break
+        if met:
+            res = i + 1
+            break
     print(res)
