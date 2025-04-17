91784c153bc705abb86873a70e9b0b152f604eca
Abdi Dabala <abdid.yadata@gmail.com>
2025-04-17 05:09:31 -0700

    Games

--- Games.py
+++ Games.py
@@ -1,14 +1,14 @@
 n = int(input())
 teams = []
-for _ in range(n):
+for i in range(n):
     h, a = map(int, input().split())
     teams.append((h, a))
 
 count = 0
-for i in range(n):
-    for j in range(n):
-        if i != j:
-            if teams[i][0] == teams[j][1]:
+for j in range(n):
+    for k in range(n):
+        if j != k:
+            if teams[j][0] == teams[k][1]:
                 count += 1
 
 print(count)
