b92516fe6037a2d06d0952718170e05587e25f1e
Abdi Dabala <abdid.yadata@gmail.com>
2025-07-06 05:15:58 -0700

    Way Too Long Words

--- codeforcegit/Way Too Long Words.py
+++ codeforcegit/Way Too Long Words.py
@@ -1,2 +1,14 @@
+n = int(input())
 
+results = []
+for word in words:
+    if len(word) > 10:
+        short = word[0] + str(len(word) - 2) + word[-1]
+        results.append(short)
+    else:
+        results.append(word)
+
+print("\n".join(results))
+
+    
 " Way Too Long Words "
