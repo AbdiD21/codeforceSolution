c06c56035523ad7292cd8cfed71c921dd31d2a4d
Abdi Dabala <abdid.yadata@gmail.com>
2025-07-06 05:33:16 -0700

    W

--- codeforcegit/Way Too Long Words.py
+++ codeforcegit/Way Too Long Words.py
@@ -1,19 +1,17 @@
 n = int(input())
-#words = [input() for _ in range(n)]
+
 words = []
-for _ in range(n):
+for count in range(n):
     word = input()
     words.append(word)
+#words = [input() for _ in range(n)]
 
 results = []
 for word in words:
     if len(word) > 10:
-        short = word[0] + str(len(word) - 2) + word[-1]
-        results.append(short)
+        abbreviated  = word[0] + str(len(word) - 2) + word[-1]
+        results.append(abbreviated )
     else:
         results.append(word)
 
 print("\n".join(results))
-
-
-" Way Too Long Words "
