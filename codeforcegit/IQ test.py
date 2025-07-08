6f6f56d377f1c7f8db2924ca0c70b31b35c70488
Abdi Dabala <abdid.yadata@gmail.com>
2025-07-07 21:38:59 -0700

    IQ test

--- codeforcegit/IQ test.py
+++ codeforcegit/IQ test.py
@@ -4,10 +4,7 @@ numbers = list(map(int, input().split()))
 parity = []
 for num in numbers:
     parity.append(num % 2)
-# Alternatively, you can use a list comprehension to create the parity list
-# parity = [num % 2 for num in numbers]
 
-# Check which parity is the minority and return its index + 1 (1-based indexing)
 if parity.count(0) == 1:
     print(parity.index(0) + 1)
 else:
